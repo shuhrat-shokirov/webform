c83e2171fa9177806b1364ddacdc054753c259a5
Shuhrat.Shokirov <avatar_9999@list.ru>
2020-03-06 16:50:13 +0300

    fix

--- cmd/crud/main.go
+++ cmd/crud/main.go
@@ -13,9 +13,9 @@ import (
 )
 
 var (
-	host = flag.String("host", "0.0.0.0", "Server host")
-	port = flag.String("port", "9999", "Server port")
-	dsn  = flag.String("dsn", "postgres://app:pass@localhost:5432/app", "Postgres DSN")
+	host = flag.String("host", "", "Server host")
+	port = flag.String("port", "", "Server port")
+	dsn  = flag.String("dsn", "", "Postgres DSN")
 )
 const envHost = "HOST"
 const envPort = "PORT"
