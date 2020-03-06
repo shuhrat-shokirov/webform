239199645aa628a3286b457cd88b4b8814e8f3f0
Shuhrat.Shokirov <avatar_9999@list.ru>
2020-03-06 09:29:43 +0300

    fix errors

--- pkg/crud/errors/errors.go
+++ pkg/crud/errors/errors.go
@@ -1,4 +1,20 @@
 package errors
 
-// TODO: посмотреть проект с лекций
+import "fmt"
 
+type mError struct {
+	text string
+	err error
+}
+
+func ApiError(text string, err error) *mError {
+	return &mError{text: text, err: err}
+}
+
+func (receiver *mError) Error() string {
+	return fmt.Sprintf("error: %v", receiver.err.Error())
+}
+
+func (receiver *mError) Unwrap() error {
+	return receiver.err
+}
